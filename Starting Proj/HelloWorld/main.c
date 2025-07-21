873513e6ca5a76ea9057637b061c08345e3dbba0
bigmanrook <wells.jude0208@gmail.com>
2025-07-21 14:49:48 +0200

    Added HelloWorld file

---
+++ Starting Proj/HelloWorld/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float bankaccount = 500.55;
+    int date = 2, month = 8, year = 2006;
+    char myName[] = "Jude";
+    printf("Hello world!\n");
+    printf("My name is: %s\n", myName);
+    printf("my favorite number is: %f", bankaccount);
+    return 0;
+}
