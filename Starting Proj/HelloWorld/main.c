848478f2437acc1c64cbb7d479c87ddc7d59982a
bigmanrook <wells.jude0208@gmail.com>
2025-07-21 18:50:07 +0200

    Worked on C course, nearly complete with tutorial - added if statements, case statements, and operators along with different data types

--- Starting Proj/HelloWorld/main.c
+++ Starting Proj/HelloWorld/main.c
@@ -3,11 +3,59 @@
 
 int main()
 {
-    float bankaccount = 500.55;
+    float bankaccount = 500.55, expense = 300.5; /*Testing out variables and data types in C*/
     int date = 2, month = 8, year = 2006;
-    char myName[] = "Jude";
+    const char myName[] = "Jude";
     printf("Hello world!\n");
     printf("My name is: %s\n", myName);
-    printf("my favorite number is: %f", bankaccount);
+    printf("my birthday is: %d of %d in the year %d\n", date, month, year);
+    if (bankaccount>expense) {
+        printf("Well done!\n");
+    } else {
+        printf("Cut down on expenditure!\n")
+;    } /*Testing out if and else statements - do not have an if else statement, but similar syntax to if statement*/
+    int time = 20;
+    (time < 18) ? printf("Good day.\n") : printf("Good evening.\n"); /*Testing out ternary operator*/
+    switch (month) {
+        case 1:
+            printf("Jan");
+            break;
+        case 2:
+            printf("Feb");
+            break;
+        case 3:
+            printf("Mar");
+            break;
+        case 4:
+            printf("Apr");
+            break;
+        case 5:
+            printf("May");
+            break;
+        case 6:
+            printf("Jun");
+            break;
+        case 7:
+            printf("July");
+            break;
+        case 8:
+            printf("Aug");
+            break;
+        case 9:
+            printf("Sep");
+            break;
+        case 10:
+            printf("Oct");
+            break;
+        case 11:
+            printf("Nov");
+            break;
+        case 12:
+            printf("Dec");
+            break;
+        default:
+            printf("Tf month you born in cuz?");
+            break;
+    } /*Testing out switch and case statements*/
     return 0;
 }
